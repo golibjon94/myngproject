3329601f2f49c8a7910310cb1a763c1f62a14b7c
User <golib94jon@gmail.com>
2020-12-03 14:30:45 +0500

    first commit

---
+++ src/app/selector/selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DateService } from '../shared/date.service';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.scss']
+})
+export class SelectorComponent {
+  constructor(private dateService: DateService) { }
+  }
+
